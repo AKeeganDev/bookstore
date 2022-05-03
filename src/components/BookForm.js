27ce18fa5c609fe92b638bc3ce2afeee27cff12e
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-03 19:00:26 -0400

    Setup addBook functionality using Redux state management

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,13 +1,31 @@
-const BookForm = () => (
-  <form action="submit">
-    <label htmlFor="title">
-      <input type="text" name="title" id="title" placeholder="Add Title..." />
-    </label>
-    <label htmlFor="title">
-      <input type="text" name="author" id="author" placeholder="Add Author..." />
-    </label>
-    <button onClick={(e) => { e.preventDefault(); }} type="submit">Add Book</button>
-  </form>
-);
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
+
+const BookForm = () => {
+  const dispatch = useDispatch();
+  return (
+    <form action="submit">
+      <label htmlFor="title">
+        <input type="text" name="title" id="title" placeholder="Add Title..." />
+      </label>
+      <label htmlFor="title">
+        <input type="text" name="author" id="author" placeholder="Add Author..." />
+      </label>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          const newTitle = e.target.parentElement.title.value;
+          const newAuthor = e.target.parentElement.author.value;
+          dispatch(addBook(newTitle, newAuthor));
+          e.target.parentElement.title.value = '';
+          e.target.parentElement.author.value = '';
+        }}
+        type="submit"
+      >
+        Add Book
+      </button>
+    </form>
+  );
+};
 
 export default BookForm;
--- src/pages/Bookshelf.js
+++ src/pages/Bookshelf.js
@@ -1,26 +1,34 @@
+import { useSelector } from 'react-redux';
 import Book from '../components/Book';
 import BookForm from '../components/BookForm';
 
-const state = {
-  books: [
-    {
-      title: 'Aaron is Great',
-      author: 'Aaron THE Great',
-    },
-  ],
-};
+// const state = {
+//   books: [
+//     {
+//       title: 'Aaron is Great',
+//       author: 'Aaron THE Great',
+//     },
+//   ],
+// };
 
 const Bookshelf = () => {
-  const { title, author } = state.books[0];
-
+  // const { title, author } = state.books[0];
+  const books = useSelector((state) => state.bookshelf);
+  // const { title, author } = books.books[0];
   return (
     <>
       <ul className="bookshelf">
-        <Book title={title} author={author} />
+        {
+          books.books.map((book) => (
+            <li key={book.id}>
+              <Book title={book.title} author={book.author} />
+            </li>
+          ))
+        }
+        {/* <Book title={title} author={author} /> */}
       </ul>
       <BookForm />
     </>
   );
 };
-
 export default Bookshelf;
