7edb35e5028d02fe0e78c2197a164074ae1a8aa9
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-04 22:42:52 -0400

    setup access to API. Page loads with books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,11 +1,33 @@
 import { v4 as uniqueID } from 'uuid';
 
+const appID = 'Xhkn3lTXOrsLse679mJl';
+const allBooksURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appID}/books`;
+
 const ADD_BOOK = 'bookstore/book/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/book/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/book/GET_BOOKS';
 
 // sets initial default state for the shelf (empty);
-const initialState = {
-  books: [],
+
+export const fetchBooks = async () => {
+  const response = await fetch(
+    allBooksURL,
+  );
+  const data = await response.json();
+  const books = [];
+  Object.entries(data).forEach((book) => {
+    books.push(...book[1]);
+    console.log({ ...book[1], id: book[0] });
+  });
+  return books;
+};
+
+export const getBooksFromAPI = () => async (dispatch) => {
+  const books = await fetchBooks();
+  dispatch({
+    type: GET_BOOKS,
+    payload: books,
+  });
 };
 
 export const addBook = (newTitle, newAuthor) => {
@@ -14,6 +36,10 @@ export const addBook = (newTitle, newAuthor) => {
     type: ADD_BOOK,
     payload: { id: newID, title: newTitle, author: newAuthor },
   };
+};
+
+const initialState = {
+  books: [],
 };
 
 export const removeBook = (id) => ({
@@ -29,6 +55,9 @@ export default (state = initialState, action) => {
     case REMOVE_BOOK: {
       const filteredBooks = state.books.filter((booke) => booke.id !== action.payload);
       return { ...state, books: [...filteredBooks] };
+    }
+    case GET_BOOKS: {
+      return { ...state, books: [...action.payload] };
     }
     default:
       return state;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { applyMiddleware } from 'redux';
 import Books from './books/books';
 import Categories from './categories/categories';
 
 const rootReducer = combineReducers({ bookshelf: Books, checkStatus: Categories });
-const store = configureStore({ reducer: rootReducer });
+const middleWare = applyMiddleware(thunk, logger);
+const store = configureStore({ reducer: rootReducer }, middleWare);
 
 export default store;
